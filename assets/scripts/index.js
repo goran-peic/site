0fe9422591121fdb401904814b16dd34c8c31ff2
NatasaPeic <peic.natasa@gmail.com>
2016-09-06 16:35:22 -0400

    Add font

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,11 +6,9 @@
 // use require without a reference to ensure a file is bundled
 require('./example');
 
-
-
-$(  () => {
-
-
-
-
-});
+require('../../js/bootstrap.min');
+require('../../js/queryloader.min');
+require('../../js/debouncer');
+require('../../js/wow.min');
+require('../../js/backstretch.min');
+require('../../js/theme');
